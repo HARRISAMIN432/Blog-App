8cb0674ceb078d88d1d335662255c811e8c93f06
Harris <harrisaminjutt@gmail.com>
2025-07-22 16:10:30 +0500

    Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Layout from "./pages/Admin/Layout";
+import Dashboard from "./pages/Admin/Dashboard";
+import AddBlog from "./pages/Admin/AddBlog";
+import Comments from "./pages/Admin/Comments";
+import ListBlog from "./pages/Admin/ListBlog";
 
 const App = () => {
   return (
@@ -9,6 +14,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="comments" element={<Comments />} />
+          <Route path="listBlog" element={<ListBlog />} />
+        </Route>
       </Routes>
     </div>
   );
