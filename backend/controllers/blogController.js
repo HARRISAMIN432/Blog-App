fe8708ce204b0deec3d8feedf9093f49452bdc0b
Harris <harrisaminjutt@gmail.com>
2025-07-24 14:26:24 +0500

    blog controllers and routes

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -17,19 +17,59 @@ exports.addBlog = async (req, res, next) => {
     fileName: imageFile.originalname,
     folder: "/blogs",
   });
-  const optimizedImageUrl = imagekitResponse.url;
-  const imageUrl = optimizedImageUrl;
+  const image = imagekitResponse.url;
   const blog = await Blog.create({
     title,
     subtitle,
     description,
     category,
-    image: imageUrl,
-    isPublished: isPublished === "true",
+    image,
+    isPublished: isPublished === true,
   });
   if (!blog) return next(new ErrorHandler("Blog creation failed", 500));
   res.status(201).json({
     success: true,
     message: "Blog added successfully",
+  });
+};
+
+exports.getAllBlogs = async (req, res, next) => {
+  const blogs = await Blog.find();
+  if (!blogs) return next(new ErrorHandler("No blogs found", 404));
+  res.status(200).json({
+    success: true,
+    blogs,
+  });
+};
+
+exports.deleteBlog = async (req, res, next) => {
+  const { id } = req.body;
+  const blog = await Blog.findByIdAndDelete(id);
+  if (!blog) return next(new ErrorHandler("Blog not found", 404));
+  res.status(200).json({
+    success: true,
+    message: "Blog deleted successfully",
+  });
+};
+
+exports.getBlogById = async (req, res, next) => {
+  const { id } = req.params;
+  const blog = await Blog.findById(id);
+  if (!blog) return next(new ErrorHandler("Blog not found", 404));
+  res.status(200).json({
+    success: true,
+    blog,
+  });
+};
+
+exports.togglePublish = async (req, res, next) => {
+  const { id } = req.body;
+  const blog = await Blog.findById(id);
+  if (!blog) return next(new ErrorHandler("Blog not found", 404));
+  blog.isPublished = !blog.isPublished;
+  await blog.save();
+  res.status(200).json({
+    success: true,
+    message: "Blog updated successfully",
   });
 };
--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const { catchAsyncErrors } = require("../utils/catchAsyncErrors");
 const router = express.Router();
-const { addBlog } = require("../controllers/blogController");
+const {
+  addBlog,
+  getAllBlogs,
+  deleteBlog,
+  getBlogById,
+  togglePublish,
+} = require("../controllers/blogController");
 const upload = require("../middlewares/multer");
 const auth = require("../middlewares/auth");
 
 router.post("/add", upload.single("image"), auth, catchAsyncErrors(addBlog));
+router.get("/all", catchAsyncErrors(getAllBlogs));
+router.get("/:id", catchAsyncErrors(getBlogById));
+router.delete("/delete", auth, catchAsyncErrors(deleteBlog));
+router.post("/toggle-publish", auth, catchAsyncErrors(togglePublish));
 
 module.exports = router;
