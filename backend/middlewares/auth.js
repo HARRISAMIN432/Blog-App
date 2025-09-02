d78fd91f6ebf658d47a7f87be9dc78c5ba5a57e6
HarrisAmin <harrisaminjutt@gmail.com>
2025-09-02 19:00:04 +0500

    final fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.auth = async (req, res, next) => {
-  const token = req.headers.authorization;
-  console.log(token);
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
