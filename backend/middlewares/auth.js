a63644346ac3be320410b811ac3989747b52bcc0
HarrisAmin <harrisaminjutt@gmail.com>
2025-09-02 18:41:13 +0500

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 
 exports.auth = async (req, res, next) => {
   const token = req.headers.authorization;
+  console.log(token);
   if (!token) return res.status(401).json({ message: "Unauthorized" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
