82a361c8d4781a2100e93a2c40fde66373165625
HarrisAmin <harrisaminjutt@gmail.com>
2025-09-02 15:11:29 +0500

    ok

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,6 @@ const User = require("../models/User");
 exports.auth = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) return res.status(401).json({ message: "Unauthorized" });
-  console.log(token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded);
@@ -23,7 +22,6 @@ exports.auth = async (req, res, next) => {
         role: "user",
       };
     }
-    console.log(req.user);
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid Token" });
